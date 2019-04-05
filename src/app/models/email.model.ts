79cee5225f3f2f4d65bc8ec417342746529bef07
Young Liu <youngliu92@gmail.com>
2019-04-05 16:21:06 -0700

    add function to loop through the map.

--- src/app/models/email.model.ts
+++ src/app/models/email.model.ts
@@ -5,4 +5,12 @@ export class Email{
   public labels: string[] = [];
 
   constructor(public from: string, public fromEmail: string, public to: Map<string, string>, public subject: string, public body: string, public date: Date, public id: number){}
+
+  getRecipients(){
+    let recipientList: string = "";
+    this.to.forEach(function(value, key){
+      recipientList += `${key} (${value}) `;
+    });
+    return recipientList;
+  }
 }
