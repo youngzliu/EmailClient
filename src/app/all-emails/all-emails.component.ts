b41ac4b5db3eb6d98dc3d8900cdad2a1153e6839
Young Liu <youngliu92@gmail.com>
2019-04-05 14:17:36 -0700

    add service to emails component to provide email data.

--- src/app/all-emails/all-emails.component.ts
+++ src/app/all-emails/all-emails.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Email } from "../models/email.model";
+import { EmailService } from "../email.service";
 
 @Component({
   selector: 'app-all-emails',
   templateUrl: './all-emails.component.html',
-  styleUrls: ['./all-emails.component.css']
+  styleUrls: ['./all-emails.component.css'],
+  providers: [EmailService]
 })
 export class AllEmailsComponent implements OnInit {
 
-  constructor() { }
+  emails: Email[];
+
+  constructor(private emailService: EmailService) { }
 
   ngOnInit() {
+    this.emails = this.emailService.getEmails();
   }
 
 }
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,16 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SplashComponent } from './splash/splash.component'
+import { AllEmailsComponent } from './all-emails/all-emails.component';
 
 const appRoutes: Routes = [
   {
     path: "",
     component: SplashComponent
+  },
+  {
+    path: "emails",
+    component: AllEmailsComponent
   }
 ];
 
