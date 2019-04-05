a81d90f3ba3133045e2924a8f74e8ec9ec900601
Young Liu <youngliu92@gmail.com>
2019-04-05 11:32:54 -0700

    add mock email data to mock email file. Taking a lunch break!

--- src/app/mock-emails.ts
+++ src/app/mock-emails.ts
@@ -1,1 +1,6 @@
 import { Email } from "./models/email.model";
+
+export const EMAILS: Email[] = [
+  new Email("bob@gmail.com", ["susan@gmail.com"], "About next week's party",  "Hey what should I bring to the party?", new Date()),
+  new Email("dave@gmail.com", ["sarah@gmail.com"], "Homework due tomorrow",  "What is the homework for tomorrow?", new Date()),
+];
