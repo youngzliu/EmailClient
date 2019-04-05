07e2e285e1103b838b42a70cfa038d50fe6da509
Young Liu <youngliu92@gmail.com>
2019-04-05 15:52:43 -0700

    add map object to email model to store name and email addresses of recipients.

--- src/app/mock-emails.ts
+++ src/app/mock-emails.ts
@@ -1,8 +1,8 @@
 import { Email } from "./models/email.model";
 
 export const EMAILS: Email[] = [
-  new Email("bob@gmail.com", ["susan@gmail.com"], "About next week's party",  "Hey what should I bring to the party?", new Date(), 1),
-  new Email("dave@gmail.com", ["sarah@gmail.com"], "Homework due tomorrow",  "What is the homework for tomorrow?", new Date(), 2),
-  new Email("emily@gmail.com", ["kyle@gmail.com"],  "Meet up", "We should hang out tomorrow!", new Date(), 3),
-  new Email("samantha@gmail.com", ["rob@gmail.com", "eva@hotmail.com", "chelsea@yahoo.com"], "Homework due tomorrow",  "What is the homework for tomorrow?", new Date(), 4),
+  new Email("Bob", "bob@gmail.com", new Map([["Susan", "susan@gmail.com"]]), "About next week's party",  "Hey what should I bring to the party?", new Date(), 1),
+  new Email("Dave", "dave@gmail.com", new Map([["Sarah", "sarah@gmail.com"]]), "Homework due tomorrow",  "What is the homework for tomorrow?", new Date(), 2),
+  new Email("Emily", "emily@gmail.com", new Map([["Kyle", "Kyle@gmail.com"]]),  "Meet up", "We should hang out tomorrow!", new Date(), 3),
+  new Email("Samantha", "samantha@gmail.com", new Map([["Rob", "rob@gmail.com"], ["Eva", "eva@hotmail.com"], ["Chelsea", "chelsea@yahoo.com"]]), "Team Week",  "We should talk about our Team week project soon.", new Date(), 4),
 ];
