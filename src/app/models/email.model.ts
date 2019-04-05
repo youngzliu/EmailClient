ba689cc16329fa4be3b02751ec20789aef12c12e
Young Liu <youngliu92@gmail.com>
2019-04-05 11:03:42 -0700

    add default properties to email model.

--- src/app/models/email.model.ts
+++ src/app/models/email.model.ts
@@ -1,3 +1,8 @@
 export class Email{
+  public starred: boolean = false;
+  public trash: boolean = false;
+  public sent: boolean = false;
+  public labels: string[] = [];
+  
   constructor(public from: string, public to: string[], public subject: string, public body: string, public date: Date){}
 }
