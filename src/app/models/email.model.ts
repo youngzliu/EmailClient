c3840e52d15209eb2ae8af850a3595d11b51e871
Young Liu <youngliu92@gmail.com>
2019-04-20 12:43:37 -0700

    switched default values in model to optional parameters.

--- src/app/models/email.model.ts
+++ src/app/models/email.model.ts
@@ -1,10 +1,6 @@
 export class Email{
-  public starred: boolean = false;
-  public trash: boolean = false;
-  public sent: boolean = false;
-  public labels: string[] = [];
 
-  constructor(public from: string, public fromEmail: string, public to: Map<string, string>, public subject: string, public body: string, public date: Date, public id: number){}
+  constructor(public from: string, public fromEmail: string, public to: Map<string, string>, public subject: string, public body: string, public date: Date, public id: number, public starred: boolean = false, public trash: boolean = false; public sent: boolean = false, public labels: string[] = []){}
 
   getRecipients(){
     let recipientList: string = "";
