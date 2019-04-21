f20939f47e07e6bfcf638006aa67fb0774ece9e6
Young Liu <youngliu92@gmail.com>
2019-04-20 17:23:15 -0700

    add a mock json file for use.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,18 @@ import { routing } from "./app.routing";
 import { AllEmailsComponent } from './all-emails/all-emails.component';
 import { FormsModule } from "@angular/forms";
 import { EmailDetailComponent } from './email-detail/email-detail.component';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket,
+  projectId: masterFirebaseConfig.projectId,
+  messagingSenderId: masterFirebaseConfig.messagingSenderId
+};
 
 @NgModule({
   declarations: [
@@ -18,7 +29,9 @@ import { EmailDetailComponent } from './email-detail/email-detail.component';
   imports: [
     BrowserModule,
     routing,
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
